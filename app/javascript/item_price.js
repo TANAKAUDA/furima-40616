2bb18f0bda1514a93ccfcd98ad9be532fd50dde3
TANAKAUDA <hnhkn283@gmail.com>
2024-05-17 20:31:28 +0900

    商品出品機能２

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,16 +1,17 @@
+
 document.addEventListener('DOMContentLoaded', () => {
-  const priceInput = document.getElementById("item-price");
-  const addTaxPrice = document.getElementById("add-tax-price");
-  const profit = document.getElementById("profit");
+  const priceInput = document.getElementById('item-price');
+  const addTaxDom = document.getElementById('add-tax-price');
+  const profitDom = document.getElementById('profit');
 
   if (priceInput) {
-    priceInput.addEventListener("input", () => {
+    priceInput.addEventListener('input', () => {
       const inputValue = priceInput.value;
       const tax = Math.floor(inputValue * 0.1);
-      const profitValue = Math.floor(inputValue - tax);
-
-      addTaxPrice.innerHTML = tax;
-      profit.innerHTML = profitValue;
+      const profit = inputValue - tax;
+      
+      addTaxDom.innerHTML = tax;
+      profitDom.innerHTML = profit;
     });
   }
 });
